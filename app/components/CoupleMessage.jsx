9ba83f04a21a10696c0ea04f5ae23565d9943b67
fardeenpathan <md.fardeen5236@gmail.com>
2026-09-01 13:30:16 +0530

    update code

--- app/components/CoupleMessage.jsx
+++ app/components/CoupleMessage.jsx
@@ -2,16 +2,15 @@ export default function CoupleMessage() {
   return (
     <div className="bg-[url('/assets/respo_bg_twon22.webp')] md:bg-[url('/assets/bg_twon2.webp')] bg-cover bg-no-repeat">
       <div className="h-230 md:h-245 lg:h-480 3xl:h-570">
-      
-
         <div className="flex justify-center md:justify-end md:pr-5 lg:pr-10 3xl:pr-30 lg:pl-240 3xl:pl-300 md:pl-120 ">
           <div className="3xl:mt-125 mt-15">
-            <h2 className="font-cormorant-upright font-semibold text-center text-[16px] md:text-2xl lg:text-[24px] uppercase text-[#15528A] pt-0 md:pt-70 lg:pt-202 3xl:pt-150 md:leading-8 lg:leading-14 leading-8 px-10 md:px-0">
+            {/* <h2 className="font-cormorant-upright font-semibold text-center text-[16px] md:text-2xl lg:text-[24px] uppercase text-[#15528A] pt-0 md:pt-70 lg:pt-202 3xl:pt-150 md:leading-8 lg:leading-14 leading-8 px-10 md:px-0">
               We cannot Wait
-            </h2>
+            </h2> */}
 
-            <h2 className="font-cormorant-upright uppercase font-bold text-center text-2xl md:text-2xl lg:text-[34px] text-[#15528A] md:leading-8 lg:leading-14 leading-8 px-10 md:px-0">
-              To Celebrate With You
+            <h2 className="font-cormorant-upright uppercase font-bold text-center text-xl md:text-2xl lg:text-[36px] text-[#15528A] md:leading-8 lg:leading-14 leading-8 px-10 md:px-0 pt-0 md:pt-70 lg:pt-180 3xl:pt-100">
+              WE LOOK FORWARD TO <br />
+              CELEBRATING THIS BEAUTIFUL BEGINNING WITH YOU
             </h2>
             <div className="flex justify-center items-center">
               <img
@@ -22,9 +21,8 @@ export default function CoupleMessage() {
                 className="md:w-1/5 lg:w-1/2 w-1/2 h-auto"
               />
             </div>
-            <h2 className="font-cormorant-upright font-semibold text-center text-[14px] md:text-2xl lg:text-[24px] text-[#15528A] md:leading-8 lg:leading-14 leading-4 px-10  py-3 md:px-0">
-              Your love, blessing and presence will make our celebration
-              complete
+            <h2 className="font-cormorant-upright font-semibold text-center text-[14px] md:text-2xl lg:text-[24px] text-[#15528A] md:leading-8 lg:leading-14 leading-4 px-10  py-3 md:px-0 italic">
+              With love & warmest wishes,
             </h2>
             <div className="flex justify-center items-center">
               <img
--- app/page.tsx
+++ app/page.tsx
@@ -45,22 +45,31 @@ export default function Home() {
 
 
     {
+      title:"AN EVENING OF TOASTS & TOGETHERNESS",
       title_ceremony: "Cocktail",
-      subtitle: "An evening of cocktails, music & celebration",
+      subtitle: <>An intimate evening of fine spirits, music, <br /> laughter and cherished company</>,
       image: "/assets/cocktail_n.png",
+      venue_name: "IRAIUXE,",
       venue_date: "Wednesday, 28 October 2026 ",
       venue_time: " 9:00 PM Onwards",
-      venue_address: <>Farm house No. 5, Khasra No.1508, <br /> Bhatti mines, Asola, New Delhi<br /></>,
+      venue_address: <>Farm house No. 5, <br /> Khasra No.1508, Bhatti mines, <br />  Asola, New Delhi<br /></>,
       link: "https://maps.app.goo.gl/bEHnAZNQU9e7tqZk6",
     },
 
     {
+      title:"THE SACRED BEGINNING",
       title_ceremony: "Anand Karaj",
-      subtitle: "The Sacred union",
+      subtitle: <>In the presence of Waheguru Ji, <br />
+        two souls come together and two families become one.</>,
+      venue_text: <>We invite you to join us as Daizy and Manraj <br />
+        take the first steps of their journey as one, <br />
+        surrounded by the prayers, blessings and love <br />
+        of those who mean the most.</>,
       image: "/assets/anand.png",
+      venue_name: "Gurdwara Singh Sabha",
       venue_date: "Friday, 30th October 2026 ",
       venue_time: "11 AM Onwards",
-      venue_address: <>Gurdwara Singh Sabha<br /> M49J+W56, Rd Number 50, <br/>Punjabi Bagh, Delhi, 110026</>,
+      venue_address: <>M49J+W56, Rd Number 50, <br />Punjabi Bagh, New Delhi</>,
       link: "https://maps.app.goo.gl/XvTQ5BonxsDZeE1m7",
     },
 
@@ -131,9 +140,9 @@ export default function Home() {
 
         <RoseHeroTemp />
 
-        <div className="pt-38 md:pt-95 lg:pt-150 3xl:pt-00 relative z-10">
+        <div className="pt-38 md:pt-105 lg:pt-150 3xl:pt-00 relative z-10">
           <h2 className="text-[#AE633A] text-center leading-tight text-xl md:text-5xl lg:text-[80px] pb-120
-                          md:pb-250 lg:pb-450 3xl:pb-600 flex flex-col items-center gap-y-0 lg:gap-y-5">
+                          md:pb-280 lg:pb-450 3xl:pb-500 flex flex-col items-center gap-y-0 lg:gap-y-5">
 
             <span className="font-playfair-display italic">Manraj Singh Somal</span>
 
@@ -153,20 +162,32 @@ export default function Home() {
               alt="idol" width={100} height={100}
               className="w-20 h-15.5 md:w-32 md:h-25 lg:w-40 lg:h-31 object-cover" />
 
-            <h2 className="text-[#15528A] text-sm md:text-xl lg:text-3xl md:pt-8 font-playfair-display">
-              With the blessings of Waheguru
+            <h2 className="text-[#15528A] text-sm md:text-xl lg:text-2xl md:pt-8 font-playfair-display font-semibold">
+              WITH THE BLESSINGS OF WAHEGURU JI
+              <br />
+              <span className="text-[#15528A] text-xl md:text-xl lg:text-3xl font-playfair-display font-semibold">
+                A CELEBRATION OF LOVE, FAMILY & NEW BEGINNINGS</span>
             </h2>
-            <Image
+
+            <h2 className="text-[#15528A] text-xs md:text-lg lg:text-[26px] font-playfair-display">
+              Some moments are destined to become cherished memories.
+            </h2>
+            <h2 className="text-[#15528A] text-xs md:text-lg lg:text-[26px] font-playfair-display">
+              With immense joy and gratitude, <br />
+              the Singh family requests the pleasure of your presence as
+            </h2>
+
+            {/* <Image
               src="/assets/arrow.png"
               alt="arrow"
               width={2048}
               height={253}
               className="md:w-1/5 w-1/2 h-auto"
-            />
+            /> */}
 
 
             {/* <hr className="w-16 lg:w-24 border-[#15528A] my-2 md:my-4" /> */}
-            <h2 className="text-[#15528A] text-xs md:text-lg lg:text-[26px] font-playfair-display">
+            {/* <h2 className="text-[#15528A] text-xs md:text-lg lg:text-[26px] font-playfair-display">
               The Singh Family
             </h2>
           </div>
@@ -174,62 +195,82 @@ export default function Home() {
           <div className="mt-8 text-center">
             <h2 className="text-[#15528A] font-playfair-display text-3xl md:text-5xl lg:text-6xl leading-tight lg:tracking-wide tracking-wider">
               INVITES
-            </h2>
+            </h2> */}
 
-            <p className="text-[#15528A] font-playfair-display text-sm md:text-xl lg:text-3xl mt-6">
+            {/* <p className="text-[#15528A] font-playfair-display text-sm md:text-xl lg:text-3xl mt-6">
               Joyfully invites you to celebrate
-            </p>
+            </p> */}
 
             <h2 className="text-[#15528A] font-playfair-display text-center mt-14 text-4xl md:text-6xl lg:text-[100px] leading-tight font-semibold">
-              Manraj Singh Somal
+              Daizy Singh
             </h2>
 
-            <p className="text-[#15528A] font-playfair-display text-sm md:text-xl lg:text-3xl mt-4 italic">
-              Son of Gurnam Singh Somal & Jaswinder Kaur Somal
+            <p className="text-[#15528A] font-playfair-display text-sm md:text-xl lg:text-3xl mt-0 italic">
+              Daughter of Tajish Kaur & Late Jagmohan Singh
             </p>
 
             <h2 className="text-[#15528A] font-playfair-display text-center mt-4 text-4xl md:text-6xl lg:text-[100px] leading-tight font-semibold">
               <span className="text-[#15528A] font-playfair-display text-center lg:mt-10 mt-0 text-4xl md:text-6xl lg:text-[100px] leading-tight">
-                & </span>   <br /> Daizy Singh
+                & </span>   <br />Manraj Singh Somal
             </h2>
 
-            <p className="text-[#15528A] font-playfair-display text-sm md:text-xl lg:text-3xl mt-4 italic">
-              Daughter of Tajish Kaur & Late Jagmohan Singh
+            <p className="text-[#15528A] font-playfair-display text-sm md:text-xl lg:text-3xl mt-0 italic">
+              Son of Gurnam Singh Somal & Jaswinder Kaur Somal
             </p>
+
+            <h2 className="text-[#15528A] text-xs md:text-lg lg:text-[26px] font-playfair-display">
+              Begin their journey together, surrounded by the love of their families
+            </h2>
+
 
             <p className="text-[#15528A] font-playfair-display text-sm md:text-xl lg:text-3xl mt-8 font-bold">
-              THE WEDDING CELEBRATION <br /><span className="font-normal">awaiting your gracious presence</span>
+              IN CELEBRATION
             </p>
+            <h2 className="text-[#15528A] text-xs md:text-lg lg:text-[26px] font-playfair-display">
+              As two hearts unite and two families come together, <br />
+              we invite you to share in the joy of this beautiful occasion<br />
+              and raise a toast to a lifetime of love, laughter and togetherness.
+            </h2>
+            {/* <p className="text-[#15528A] font-playfair-display text-sm md:text-xl lg:text-3xl mt-8 font-bold">
+              AN EVENING OF TOASTS & TOGETHERNESS
+            </p> */}
           </div>
 
-          <div className="flex justify-center mt-20 lg:mt-40">
+          <div className="flex justify-center mt-8 lg:mt-40">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-16 lg:gap-32 3xl:gap-50">
               {events.map((event, i) => (
                 <div key={i} className="flex flex-col items-center text-center">
+                  <p className="text-[#15528A] font-playfair-display text-sm md:text-xl lg:text-3xl mt-8 font-bold">
+              {event.title}
+            </p>
                   <img
                     src={event.image}
-                    className="w-75 md:w-76 lg:w-80 3xl:w-100 h-auto rounded-2xl" />
+                    className="w-75 md:w-76 lg:w-80 3xl:w-100 h-auto rounded-2xl mt-5" />
 
-                  <h2 className="text-[#15528A] font-playfair-display text-3xl md:text-2xl lg:text-[42px] mt-4">
+                  <h2 className="text-[#15528A] font-playfair-display text-3xl md:text-2xl lg:text-[42px] mt-4 uppercase font-bold">
                     {event.title_ceremony}
                   </h2>
 
-                  <h2 className="text-[#15528A] font-playfair-display text-[16x] md:text-xl lg:text-[22px]">
+                  <h2 className="text-[#15528A] font-playfair-display text-[14x] md:text-xl lg:text-[18px] italic">
                     {event.subtitle}
+                  </h2>
+                  <h2 className="text-[#15528A] font-playfair-display text-[14x] md:text-xl lg:text-[18px] mt-2">
+                    {event.venue_text}
                   </h2>
 
                   <p className="text-[#15528A] font-playfair-display text-sm md:text-base mt-2">
                     <span className="text-sm md:text-base lg:text-xl font-semibold">{event.venue_date}</span> <br />
                     <span className="text-sm md:text-base lg:text-xl font-semibold">{event.venue_time}</span> <br />
+                    <span className="text-sm md:text-base lg:text-xl font-semibold">{event.venue_name}</span> <br />
                     <span className="text-sm md:text-base lg:text-xl">{event.venue_address}</span>
                   </p>
 
 
                   <a
                     href={event.link}
-                    className="text-[#15528A] underline md:text-sm text-[12px] mt-2 font-playfair-display font-semibold"
+                    className="text-[#15528A] md:text-sm text-[12px] mt-2 font-playfair-display font-semibold"
                     target="_blank">
-                    VIEW LOCATION
+                    VIEW LOCATION →
                   </a>
 
                 </div>
@@ -238,19 +279,40 @@ export default function Home() {
           </div>
 
 
-          <div className="relative flex flex-col items-center pt-30 lg:pt-50 3xl:pt-90 3xl:gap-60">
+
+
+          <div className="relative flex flex-col items-center pt-80 lg:pt-50 3xl:pt-90 3xl:gap-60">
             {/* Center Text */}
             {/* <div className="absolute flex flex-col justify-center text-center md:mb-0 lg:mb-0 md:top-50 lg:top-84 3xl:top-152"> */}
-            <div className="absolute left-0 right-0 w-full flex flex-col items-center justify-center text-center md:mb-0 lg:mb-0 md:top-50 lg:top-84 3xl:top-152 -mt-10">
+            <div className="absolute left-0 right-0 w-full flex flex-col items-center justify-center text-center md:mb-0 lg:mb-0 md:top-50 lg:top-84 3xl:top-152 -mt-50">
+
+              {/* <p className="text-[#15528A] font-playfair-display text-sm md:text-xl lg:text-3xl mt-8 font-bold">
+             
+            </p> */}
+
               <p className="font-playfair-display font-semibold text-xl md:text-2xl lg:text-[40px] text-[#15528A] w-60 leading-5 md:leading-10 lg:w-200 md:w-100">
-                As we begin this beautiful new chapter,
-                we would be honoured to have you by our side.
+                AND SO, IT BEGINS…
+              </p>
+              
+              <p className="text-[#15528A] font-eb-garamond text-sm md:text-xl lg:text-2xl mt-4 lg:px-100 3xl:px-150 px-0 md:px-60 italic">
+                A new home to build. <br />
+                A thousand memories to make. <br />
+                A lifetime of laughter, love and togetherness.
               </p>
 
-              <p className="text-[#15528A] font-eb-garamond text-sm md:text-xl lg:text-3xl mt-4 lg:px-100 3xl:px-150 px-28 md:px-60">
-                As we begin this beautiful new chapter,
-                we would be honoured to have you by our side.
+              <p className="text-[#15528A] font-eb-garamond text-sm md:text-xl lg:text-2xl mt-4 lg:px-100 3xl:px-150 px-0 md:px-60">
+                As they begin this journey, <br />
+                your presence will be one of the most beautiful<br />
+                parts of their celebration.
               </p>
+
+
+              <p className="text-[#15528A] font-eb-garamond text-sm md:text-xl lg:text-2xl mt-4 lg:px-100 3xl:px-150 px-0 md:px-60">
+                Come raise a glass, share a prayer,<br />
+                dance a little longer and celebrate a love<br />
+                that brings two families together.
+              </p>
+
             </div>
             <Image
               src="/assets/couple.webp"
